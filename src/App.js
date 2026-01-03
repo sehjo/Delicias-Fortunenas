5ba3c02e03416bbf7d32732274a7eb3764a6b2a1
Steph <sivv2004@gmail.com>
2026-01-03 14:27:40 -0600

    Add Google Map component with responsive styling
    Introduced a new GoogleMap component that embeds a Google Map using an iframe, along with associated CSS for responsive design. Integrated the component into App.js. Also removed an unnecessary height property from index.css.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { NavBar } from './Components/Navbar/Navbar';
+import { GoogleMap } from './Components/Map/GoogleMap';
 
 function App() {
   return (
     <div className="App">
       <NavBar/> 
+      <GoogleMap/>
       </div>
   );
 }
