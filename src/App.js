24dba2eace620a0d9479ab63e426c0917a9a443f
Steph <sivv2004@gmail.com>
2026-01-02 20:05:22 -0600

    Add footer component with contact info and styles
    Introduces a new Footer component displaying business hours, contact emails, phone numbers, and social media icons. Adds related SVG icons, updates App.js to include the footer, and applies new styles via Footer.css. Also adds the 'react-icons' dependency and links Google Fonts in index.html.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { NavBar } from './Components/Navbar';
+import { Footer } from './Components/Footer';
+
 function App() {
   return (
     <div className="App">
-      <NavBar /> 
-      Prueba react
-      </div>
+      <NavBar />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Components/Footer.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import '../styles/Footer.css';
+import { FaTiktok } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+
+
+
+
+var email= 'delifortuna.28042020@gmail.com';
+var phone1= '(506) 8390-2262';
+var phone2= '(506) 7087-7268';  
+var phone3= '(506) 8390-2262';
+
+export function Footer() {
+  return (
+    <>  
+        <footer className="footer-content">
+                <div className='column-footer'>{/*Horario*/}
+                    <h3 className='subtitle-footer'>Horario</h3>
+                    <div className='schedule-item'>
+                        <li>
+                            Lunes: 8:00 AM - 8:00 PM
+                        </li>
+                        <li>
+                            Martes: 8:00 AM - 8:00 PM
+                        </li>
+                        <li>
+                            Miercoles: 8:00 AM - 8:00 PM
+                        </li>
+                        <li>
+                            Jueves: 8:00 AM - 8:00 PM
+                        </li>
+                        <li>
+                            Viernes: 8:00 AM - 8:00 PM
+                        </li>
+                        <li>
+                            Sabado: 8:00 AM - 8:00 PM
+                        </li>
+                        <li> 
+                            Domingo: Cerrado    
+                        </li>
+                    </div>
+                </div>
+                <div  className='column-footer'>{/*Email*/}
+                    <h3 className='subtitle-footer'>Email</h3>
+                    
+                    <div className='contact-item'>
+                        <img src={`${process.env.PUBLIC_URL}/icons/email.svg`} alt="email-icon" className='contact-icon' />
+                        <p>{email}</p>
+                    </div>
+                    <div className='icons-item'>
+                        <FaTiktok />
+                        <FaInstagram />
+                        <FaFacebookF />
+                        <FaWhatsapp />
+                    </div>
+                </div>
+                <div  className='column-footer'>{/*Telefonos*/}
+                    <h3 className='subtitle-footer'>Telefonos</h3>
+                    <div className='contact-item'>
+                        <img src={`${process.env.PUBLIC_URL}/icons/phone.svg`} alt="phone-icon" className='contact-icon' />
+                        <p>{phone1}</p>
+                    </div>
+                    <div className='contact-item'>
+                        <img src={`${process.env.PUBLIC_URL}/icons/phone.svg`} alt="phone-icon" className='contact-icon' />
+                        <p>{phone2}</p>
+                    </div>
+                    <div className='contact-item'>
+                        <img src={`${process.env.PUBLIC_URL}/icons/phone.svg`} alt="phone-icon" className='contact-icon' />
+                        <p>{phone3}</p>
+                    </div>
+                </div>
+                 <div  className='column-footer'>{/*Telefonos*/}
+                    <h3 className='subtitle-footer'>Lorem Ipsum</h3>
+                    
+                </div>
+            
+        </footer>
+    </>
+  );
+}
