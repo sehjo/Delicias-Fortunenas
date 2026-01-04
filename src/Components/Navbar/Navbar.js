73b274892f9b2bfbff74fbdef6246f45e54a20aa
sehjo <johan.atehortua@ucr.ac.cr>
2026-01-03 20:04:36 -0600

    refacto: Instagram image icon with react-icons in Navbar
    Replaced the static Instagram image icon with the FaInstagram icon from react-icons in the Navbar component. Updated styles in Navbar.css for improved consistency and maintainability using CSS variables. Removed the unused instagramIcon.png asset and added react-icons as a project dependency.

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,8 +1,7 @@
 import './Navbar.css';
 import iconPymeNavbar from '../../Image/LogoEmpresa.png';
 import iconFb from '../../Image/iconFb.svg';
-import iconInst from '../../Image/instagramIcon.png'   
-  
+import { FaInstagram } from "react-icons/fa";   
 
 const pathIndex = '../../../public/index.html'
 
@@ -14,18 +13,16 @@ export function NavBar() {
                     <img alt="IconFacebook" src={iconFb} id="iconFbNavbar"/>
                 </a>
             </div>
-
             <div id="divCenter">
                 <a href= {pathIndex}> Inicio</a>
                 <a href= ""> Acerca</a>
                 <img alt="MainIcon" src={iconPymeNavbar} id="iconPymeNavbar"/>
                 <a href= ""> Productos</a>
                 <a href= ""> Contacto</a>
-            </div>
-            
+            </div>            
             <div id = "divRight">
                 <a href= "https://www.instagram.com/delicias_fortunenas/"> 
-                    <img alt="IconInstagram" src={iconInst} id="iconIgNavbar"/>
+                    <FaInstagram id="iconIgNavbar"/>
                 </a>
             </div>               
         </nav>
