09ae70c1dd6c0aa56513f9bdedca3b8fd535b488
sehjo <johan.atehortua@ucr.ac.cr>
2026-01-07 17:43:14 -0600

    feat: make the third frame
    -add the container with the text about us
    
    -add button of read more without functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import { NavBar } from './Components/Navbar/Navbar';
 import MainPageFrameStart from './Components/MainPage-FrameStart/MainPage-FrameStart';
 import MainPageSecondFrame from './Components/MainPage-SecondFrame/MainPage-SecondFrame';
 import LeafletMap from './Components/Map/LeafletMap';
+import MainPageThirdFrame from './Components/MainPage-ThirdFrame/MainPage-ThirdFrame';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/> 
-      <MainPageFrameStart/>
-      <MainPageSecondFrame/>
+      <MainPageThirdFrame/>
       <LeafletMap/>
       </div>
   );
---
+++ src/Components/MainPage-ThirdFrame/MainPage-ThirdFrame.js
@@ -0,0 +1,15 @@
+import './MainPage-ThirdFrame.css';
+
+export default function MainPageThirdFrame() {
+    return (
+        <div id="container-thirdframe">
+            <div id='text-about-us'>
+                
+                <p>Acerca de Nosotros</p>
+            </div>
+            <div>
+                <button id='btn-read-more'>Leér mas</button>
+            </div>
+        </div>
+    );
+}
