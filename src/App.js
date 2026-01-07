a0fd7c0f96854711e57bfa3599ef415d9f61955f
sehjo <johan.atehortua@ucr.ac.cr>
2026-01-07 14:03:43 -0600

    fix: bug of space between images of mainpage-secondframe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { NavBar } from './Components/Navbar/Navbar';
 import MainPageFrameStart from './Components/MainPage-FrameStart/MainPage-FrameStart';
-
+import MainPageSecondFrame from './Components/MainPage-SecondFrame/MainPage-SecondFrame';
 function App() {
   return (
     <div className="App">
       <NavBar/> 
       <MainPageFrameStart/>
+      <MainPageSecondFrame/>
     </div>
   );
 }
