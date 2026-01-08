789bb6d7798c668e1469344373b0e355e53646d9
Steph <sivv2004@gmail.com>
2026-01-07 19:19:58 -0600

    Remove Footer component and references
    Deleted Footer.js and Footer.css, and commented out Footer import and usage in App.js. This change removes the footer from the application UI.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ProductCard } from './Components/ProductCard/ProductCard';
 import bread1 from './Image/cinnamon_rolls.png';
 import bread2 from './Image/Signature_brioche_with_local_butter.png';
 import bread3 from './Image/carrot_cake.png';
-import { Footer } from './Components/Footer/Footer';
+// import { Footer } from './Components/Footer/Footer';
 import LeafletMap from './Components/Map/LeafletMap';
 
 function App() {
@@ -44,7 +44,7 @@ function App() {
       </div>
       
       {/* Footer */}
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
