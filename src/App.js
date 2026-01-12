10452943b390f294c32bd6b7d97eec9e27c3e89b
sehjo <johan.atehortua@ucr.ac.cr>
2026-01-12 17:40:20 -0600

    Add missing components to App and update FindUs styles
    Added NavBar, MainPageFrameStart, and MainPageSecondFrame components to App.js for improved layout. Updated MainPage-ThirdFrame-FindUs.css to remove extra whitespace and add styles for #icon-maps element.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import LeafletMap from './Components/Map/LeafletMap';
 import MainPageThirdFrameAboutUs from './Components/MainPage-ThirdFrame/About-us/MainPage-ThirdFrame-AboutUs';
 import MainPageThirdFrameFindUs from './Components/MainPage-ThirdFrame/Find-Us/MainPage-ThirdFrame-FindUs';
 import MainPageFourthFrame from './Components/MainPage-FourthFrame/MainPage-FourthFrame';
+
 function App() {
   return (
     <div className="App">
-     
+      <NavBar />
+      <MainPageFrameStart />
+      <MainPageSecondFrame />
+      <MainPageThirdFrameAboutUs/>
+      <LeafletMap />
       <MainPageThirdFrameFindUs/>
       <MainPageFourthFrame/>
       </div>
